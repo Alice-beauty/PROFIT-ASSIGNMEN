7131c4ad789b2ccf479104b7626f5ffaa371ea6e
Alice-beauty <115928236+Alice-beauty@users.noreply.github.com>
2022-11-14 15:59:29 +0300

    Add files via upload

---
+++ profit code.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+// this is the start of my code
+int buyingprice ;
+int sellingprice ;
+int profit ;
+
+cout<<"my buying price is: ";
+cin>> buyingprice ;
+cout<<"my selling price is: ";
+cin>> sellingprice;
+
+// logic 
+profit = sellingprice - buyingprice;
+
+cout<<" my profit is: ";
+cout<<profit;
+
+return 0;
+}
